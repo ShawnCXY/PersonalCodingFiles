7c86f542af0140a7d66122996d75aab9c4388279
ShawnCXY <1919493486@qq.com>
2022-04-09 09:31:47 +0800

    04月09日

--- 1.1.ExercisesOfProgramming/test.py
+++ 1.1.ExercisesOfProgramming/test.py
@@ -1,49 +1,73 @@
 # coding:utf-8
 
 
-def iterativeBackTrackFunc(wList, vList, C):
-    def iterativeBackTrack():
-        t, curX = 0, [-1]*N  # 当前深度，当前路径
-        curWeight, curValue = 0, 0
-        bestValue, bestX = 0, None
-        while t >= 0:
-            if t < N and curX[t] <= 0:
-                curX[t] += 1
-                if curX[t] == 0:
-                    if t == N-1 and curValue > bestValue:
-                        bestValue = curValue
-                        bestX = curX.copy()
-                    t += 1
-                else:
-                    if (curWeight+wList[t]) <= C:
-                        curWeight += wList[t]
-                        curValue += vList[t]
-                        if t == N-1 and curValue > bestValue:
-                            bestValue = curValue
-                            bestX = curX.copy()
-                        t += 1
-                    else:
-                        curX[t] = -1
-                        t -= 1
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = s.replace(' ', '')
+        digits, operate = [], []
+        lenS = len(s)
+        i = 0
+        while i < lenS:
+            print("i:{}, s[i]:{}, digits:{}, ope:{}".format(i, s[i], digits, operate))
+            if s[i] in '0123456789':
+                j = i
+                while j < lenS and s[j] in '0123456789':
+                    j += 1
+                digits.append(int(s[i:j]))
+                i = j
             else:
-                if t >= N:
-                    t -= 1
+                if len(operate) == 0:
+                    operate.append(s[i])
+                    i += 1
+                elif s[i] in '+-' and operate[-1] in '+-':
+                    while len(operate)>0 and s[i] in '+-' and operate[-1] in '+-':
+                        right, left = digits.pop(), digits.pop()
+                        oper = operate.pop()
+                        if oper == '+':     
+                            digits.append(left+right)
+                        else:
+                            digits.append(left-right)
+                    operate.append(s[i])
+                    i += 1
+                elif s[i] in '+-' and operate[-1] in '*/':
+                    while len(operate) > 0 and s[i] in '+-' and operate[-1] in '*/':
+                        right, left = digits.pop(), digits.pop()
+                        oper = operate.pop()
+                        if oper == '*':     
+                            digits.append(left*right)
+                        else:
+                            digits.append(left//right)
+                    operate.append(s[i])
+                    i += 1
+                elif s[i] in '*/' and operate[-1] in '+-':
+                    operate.append(s[i])
+                    i += 1
                 else:
-                    curWeight -= wList[t]
-                    curValue -= vList[t]
-                    curX[t] = -1
-                    t -= 1
-        return bestValue, bestX
-    
-    wList = wList  # 每个物品的重量 
-    vList = vList  # 每个物品的价值  
-    C = C          # 背包的容量
-    N = len(wList) # 物品的数量
+                    while len(operate) > 0 and s[i] in '*/' and operate[-1] in '*/':
+                        right, left = digits.pop(), digits.pop()
+                        oper = operate.pop()
+                        if oper == '*':     
+                            digits.append(left*right)
+                        else:
+                            digits.append(left//right)
+                    operate.append(s[i])
+                    i += 1
+            print("digits:{}, ope:{}".format(digits, operate))
+        while len(operate) > 0:
+            right, left = digits.pop(), digits.pop()
+            oper = operate.pop()
+            if oper == '*':     
+                digits.append(left*right)
+            elif oper == '/':
+                digits.append(left//right)
+            elif oper == '+':
+                digits.append(left+right)
+            else:
+                digits.append(left-right)
+        return digits[0]
 
-    bestValue, bestX = iterativeBackTrack()
-    print("最优值：{}， 最优解：{}".format(bestValue, bestX))
 
-
-iterativeBackTrackFunc([2,8,10,8,5,7,3], [2,12,5,4,1,4,2], 20)
+solo = Solution()
+solo.calculate("3+2*2")
 
 
